27f2aa0fc0bcc108d8fc2281ed5998bbf5f00384
Helena Edlund <helena.edlund@astrazeneca.com>
2019-01-03 13:35:42 +0000

    merged directories and variables

--- Scripts/Setup/setup03_variables.R
+++ Scripts/Setup/setup03_variables.R
@@ -7,15 +7,90 @@
 # Dependencies: setup01_rEnvironment.R
 ###################################################
 
-# Note, this script is a little bit of a mixbag for reporting and workflow. 
-# Should probably be splitted into the appropriate parts. 
+# ------------------------------------------------------------------
+#  Directories
+# ------------------------------------------------------------------
+# This function is kept here to make it easy to add/remove. 
+# Pre-defined folder structure is the default for the AZ knowledge management
+# Its kept in a function formate not to crowd gloabl environment with all dir names separately
 
-# The column names etc used below are default from the AZ data standards. 
-# That is, it will need to be updated depending on the present data structure   
+list_directories <- function(make = F){
+  # Directories at activity level: 
+  # do not remove the leading ".", it will cause problems with latex/knitr)
+  scripts_dir       <- file.path(".", "Scripts")
+  derived_data_dir  <- file.path(".", "DerivedData")
+  model_dir         <- file.path(".", "Models")
+  report_dir        <- file.path(".", "Report")
+  results_dir       <- file.path(".", "Results")
+  sim_dir           <- file.path(".", "Simulations")
+  source_data_dir   <- file.path(".", "SourceData")
+  
+  ##sub-directories
+  #script_dir
+  setup_dir         <- file.path(scripts_dir, "Setup")
+  functions_dir     <- file.path(scripts_dir, "Functions")
+  
+  #model_dir
+  base_model_dir      <- file.path(model_dir, "BaseModel")
+  covariate_model_dir <- file.path(model_dir, "CovariateModel")
+  
+  #result_dir
+  res_other_dir       <- file.path(results_dir, "Other")
+  res_eda_dir         <- file.path(results_dir, "ExploratoryDataAnalysis")
+  res_base_model_dir  <- file.path(results_dir, "BaseModel")
+  res_cov_model_dir   <- file.path(results_dir, "CovariateModel")
+  
+  # #report_dir - to be incoporated
+  # rep_setup_dir <- file.path(report_dir, "Setup")
+  # rep_sections_dir <- file.path(report_dir, "sections")
+  # rep_appendicies_dir <- file.path(report_dir, "appendices")
+  # rep_images_dir <- file.path(report_dir, "images")
+  
+  ##list_all_directories
+  all_dir <-
+    list(
+      scripts_dir = scripts_dir,
+      derived_data_dir = derived_data_dir,
+      model_dir = model_dir,
+      report_dir = report_dir,
+      results_dir = results_dir,
+      sim_dir = sim_dir,
+      source_data_dir = source_data_dir,
+      setup_dir = setup_dir,
+      functions_dir = functions_dir,
+      base_model_dir = base_model_dir,
+      covariate_model_dir = covariate_model_dir,
+      res_other_dir = res_other_dir,
+      res_eda_dir = res_eda_dir,
+      res_base_model_dir = res_base_model_dir,
+      res_cov_model_dir = res_cov_model_dir #,
+      #rep_setup_dir = rep_setup_dir,
+      #rep_sections_dir = rep_sections_dir,
+      #rep_appendicies_dir = rep_appendicies_dir,
+      #rep_images_dir = rep_images_dir
+    )
+  
+  if(make){
+    # make directories not already created 
+    lapply(all_dir, pmworkbench::mkdirp)
+  }
+  
+  # return list of all directories
+  return(all_dir)
+} 
+
+all_dir <- list_directories()
+
+
+
+
 
 # ------------------------------------------------------------------
 #  Data
 # ------------------------------------------------------------------
+
+# The column names etc used below are default from the AZ data standards. 
+# That is, it will need to be updated depending on the present data structure   
 
 # -------------------- 
 #  Source dataset
@@ -74,6 +149,9 @@ base_cont_cov <- c("AGE","BSCR","BEGFR","BWT","BHT","BBMI")
 all_cols <- c(cols_study_related, cols_numeric, cols_factors, 
               cols_cat_cov,cols_cont_cov,base_cat_cov,base_cont_cov)
 
+
+
+
 # ------------------------------------------------------------------
 #  List lables and settings for plots used in EDA
 # ------------------------------------------------------------------
@@ -84,6 +162,10 @@ labs_conc <- paste0(dv_name," concentration (", dv_unit,")")
 
 # Re-occuring x-axis breaks
 tapd_breaks <- c(0, 2, 4, 6, 8, seq(from=12, to=200, by=6))
+
+
+
+
 
 # ------------------------------------------------------------------
 #  Save environment 
